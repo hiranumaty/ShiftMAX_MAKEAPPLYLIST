d7b91d29d48c93f7e4a17c32cfd60742ab49c4ee
hiranumaty <hiranumaty@noar.co.jp>
2021-06-25 14:02:37 +0900

    タイトルによって取得個所を変更

--- Modules/Driver.py
+++ Modules/Driver.py
@@ -1,4 +1,3 @@
-import re
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
@@ -9,31 +8,35 @@ from tkinter import messagebox
 def ConnectDriver(parent):
     '''これはChromeDriverを用いた動作を行う'''
     def getDetailData(driver,wait,link):
-        '''詳細を取得し元のページに戻る'''
+        '''詳細情報を辞書の形で取得しreturn'''
         driver.find_element_by_id(link).click()
         wait.until(expected_conditions.element_to_be_clickable((By.ID, "listShinseiView_itm14")))
         #ここで要素内容を切り出して、リストを作成しそれを返却する
         U_ID = driver.find_element_by_id('lblNo').text
         U_NAME = driver.find_element_by_id('lblName').text
         U_NAME = ''.join(U_NAME.split())[:-1]
-        #対象の日付の取得
-        #申請の種別によって、日付や理由の位置が変更されてしまう問題をいかに解決するか(申請種別を取得しそれごとに取得する内容を変更するしかない)
-        StartDate_list = [driver.find_element_by_id('listShinseiView_itm2').text,driver.find_element_by_id('listShinseiView_itm3').text,driver.find_element_by_id('listShinseiView_itm4').text,driver.find_element_by_id('listShinseiView_itm5').text,driver.find_element_by_id('listShinseiView_itm6').text]
-        D_StartDate = ''.join(StartDate_list)
+        D_title = driver.find_element_by_id('lblTitle').text
+        D_StartDate = ''
         D_ENDDate = ''
-        
-        if re.compile("\d{4}").search(driver.find_element_by_id('listShinseiView_itm9').text) and re.compile("\d{1,2}").search(driver.find_element_by_id('listShinseiView_itm11').text) and re.compile("\d{1,2}").search(driver.find_element_by_id('listShinseiView_itm13').text):
+        D_Reason = ''
+        #以下の部分をtitleを元に取得個所を変更する
+        if D_title == "打刻修正申請":
+            StartDate_list = [driver.find_element_by_id('listShinseiView_itm2').text,driver.find_element_by_id('listShinseiView_itm3').text,driver.find_element_by_id('listShinseiView_itm4').text,driver.find_element_by_id('listShinseiView_itm5').text,driver.find_element_by_id('listShinseiView_itm6').text]
+            D_StartDate = D_StartDate.join(StartDate_list)
+            D_Reason = driver.find_element_by_id('listShinseiView_itm14').text
+        elif D_title == "休暇申請":
+            StartDate_list = [driver.find_element_by_id('listShinseiView_itm2').text,driver.find_element_by_id('listShinseiView_itm3').text,driver.find_element_by_id('listShinseiView_itm4').text,driver.find_element_by_id('listShinseiView_itm5').text,driver.find_element_by_id('listShinseiView_itm6').text]
+            D_StartDate = D_StartDate.join(StartDate_list)
             ENDDate_list = [driver.find_element_by_id('listShinseiView_itm9').text,driver.find_element_by_id('listShinseiView_itm10').text,driver.find_element_by_id('listShinseiView_itm11').text,driver.find_element_by_id('listShinseiView_itm12').text,driver.find_element_by_id('listShinseiView_itm13').text]
             D_ENDDate = D_ENDDate.join(ENDDate_list)
+            D_Reason = driver.find_element_by_id('listShinseiView_itm17').text
 
-        D_title = driver.find_element_by_id('lblTitle').text
-        #終了日があると理由が取得できない問題(listShinseiView_itm14)がずれる
-        D_Reason = driver.find_element_by_id('listShinseiView_itm14').text
         Detail_Dict ={'ID':U_ID,'NAME':U_NAME,'StartDate':D_StartDate,'EndDate':D_ENDDate,'Title':D_title,'Reason':D_Reason}
         '''申請一覧のページに戻る'''
         driver.back()
         wait.until(expected_conditions.element_to_be_clickable((By.ID, "listShinseiList")))
         return Detail_Dict
+    '''以下がドライバーの基本操作'''
     if parent.St_ID.get()!='' and parent.St_PASS.get() != '':
         #seleniumの設定
         option = Options()
