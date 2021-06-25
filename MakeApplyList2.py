e0c0643b0e3b27cacf78dfa1fc68fac610227d58
hiranumaty <hiranumaty@noar.co.jp>
2021-06-25 10:34:41 +0900

    終了日日付取得完了

--- MakeApplyList2.py
+++ MakeApplyList2.py
@@ -2,7 +2,6 @@ import tkinter as tk
 import tkinter.ttk as ttk
 import asyncio
 import time
-import re
 from tkcalendar import Calendar, DateEntry
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -94,20 +93,10 @@ class MakeApplyList(tk.Frame):
         U_ID = driver.find_element_by_id('lblNo').text
         U_NAME = driver.find_element_by_id('lblName').text
         U_NAME = ''.join(U_NAME.split())[:-1]
-        #対象の日付の取得
-        StartDate_list = [driver.find_element_by_id('listShinseiView_itm2').text,driver.find_element_by_id('listShinseiView_itm3').text,driver.find_element_by_id('listShinseiView_itm4').text,driver.find_element_by_id('listShinseiView_itm5').text,driver.find_element_by_id('listShinseiView_itm6').text]
-        D_StartDate = ''.join(StartDate_list)
-        D_ENDDate = ''
-        #日付が4-2-2であるとき
-        print(re.search("\d{4}",driver.find_element_by_id('listShinseiView_itm9').text))
-        if re.compile("\d{4}").search(driver.find_element_by_id('listShinseiView_itm9').text) and re.compile("\d{1,2}").search(driver.find_element_by_id('listShinseiView_itm11').text) and re.compile("\d{1,2}").search(driver.find_element_by_id('listShinseiView_itm13').text):
-            ENDDate_list = [driver.find_element_by_id('listShinseiView_itm9').text,driver.find_element_by_id('listShinseiView_itm10').text,driver.find_element_by_id('listShinseiView_itm11').text,driver.find_element_by_id('listShinseiView_itm12').text,driver.find_element_by_id('listShinseiView_itm13').text]
-            D_ENDDate = D_ENDDate.join(ENDDate_list)
-
+        #日付(開始と終了の取得をどのように行うか)        
         D_title = driver.find_element_by_id('lblTitle').text
-        #終了日があると理由が取得できない問題(listShinseiView_itm14)がずれる
         D_Reason = driver.find_element_by_id('listShinseiView_itm14').text
-        Detail_Dict ={'ID':U_ID,'NAME':U_NAME,'StartDate':D_StartDate,'EndDate':D_ENDDate,'Title':D_title,'Reason':D_Reason}
+        Detail_Dict ={'ID':U_ID,'NAME':U_NAME,'Title':D_title,'Reason':D_Reason}
         '''申請一覧のページに戻る'''
         driver.back()
         wait.until(expected_conditions.element_to_be_clickable((By.ID, "listShinseiList")))
@@ -142,8 +131,9 @@ class MakeApplyList(tk.Frame):
             #ここで辞書を作成する
             Detail_dictList = []
             #とりあえず先頭2件のデータのリンクをクリックして情報を取得する(今後複数データを取得することになる)
-            for iterate in range(0,4):
+            for iterate in range(0,2):
                 '''申請一覧ページであるかを待つ必要がある'''
+                print(links_list[iterate])
                 Detail_dictList.append(self.getDetailData(driver,wait,links_list[iterate]))
             print(Detail_dictList)
         else:
